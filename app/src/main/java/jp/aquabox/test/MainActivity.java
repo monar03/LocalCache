16322af7c908cb69b373d122198992b29a2f8fcb
Administrator <admin@example.com>
2020-07-18 15:20:55 +0900

    add cache

--- app/src/main/java/jp/aquabox/test/MainActivity.java
+++ app/src/main/java/jp/aquabox/test/MainActivity.java
@@ -1,15 +1,65 @@
 package jp.aquabox.test;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import jp.aquabox.cache.DiskCache;
 import jp.aquabox.cache.R;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private DiskCache diskCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        try {
+            diskCache = new DiskCache(DiskLruCache.open(getCacheDir(), 1, 1, 300000));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        findViewById(R.id.in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                diskCache.set("test", new Data(), -1, TimeUnit.MINUTES);
+            }
+        });
+        findViewById(R.id.out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Disposable d = diskCache.<Data>get("test")
+                        .subscribe(
+                                new Consumer<Data>() {
+                                    @Override
+                                    public void accept(Data data) {
+                                        ((TextView) findViewById(R.id.print)).setText(data.query);
+                                    }
+                                }
+                        );
+            }
+        });
+    }
 }
+
+class Data implements Serializable {
+    public final int test = 10;
+    public final String query = "query";
+}
+
